e36f5aa17f788ac3c5e929a5f3839504cd9a300a
mzardiashvili <m_zardiashvili@outlook.com>
2024-10-19 16:39:55 +0400

    add routing, create layout, header and components for products

--- Angular.Hackathon.ChatBot/apps/ChatBot/src/app/main/app.routes.ts
+++ Angular.Hackathon.ChatBot/apps/ChatBot/src/app/main/app.routes.ts
@@ -1,3 +1,11 @@
-import { Route } from '@angular/router';
+import {Route} from '@angular/router';
+import {AccountsComponent} from "../features/products/accounts/accounts.component";
+import {CardsComponent} from "../features/products/cards/cards.component";
+import {DepositsComponent} from "../features/products/deposits/deposits.component";
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {path: '', component: AccountsComponent},
+  {path: 'accounts', component: AccountsComponent},
+  {path: 'cards', component: CardsComponent},
+  {path: 'deposits', component: DepositsComponent},
+];
--- Angular.Hackathon.ChatBot/apps/ChatBot/src/app/shared/components/layout/layout.component.ts
+++ Angular.Hackathon.ChatBot/apps/ChatBot/src/app/shared/components/layout/layout.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HeaderComponent} from "../header/header.component";
+import {NavbarComponent} from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, NavbarComponent],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.css',
+  styleUrl: './layout.component.scss',
 })
 export class LayoutComponent {}
