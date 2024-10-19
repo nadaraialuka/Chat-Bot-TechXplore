3ad3478c7a0b8dc0d51bd5cf58405e1b866884ff
mzardiashvili <m_zardiashvili@outlook.com>
2024-10-20 01:16:00 +0400

    chatbot fixes

--- Angular.Hackathon.ChatBot/apps/ChatBot/src/app/shared/components/chatbot/chatbot.component.ts
+++ Angular.Hackathon.ChatBot/apps/ChatBot/src/app/shared/components/chatbot/chatbot.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormControl, ReactiveFormsModule} from "@angular/forms";
 import {ChatService} from "../../../services/chat/chat.service";
+import {ProductsService} from "../../../services/products/products.service";
 
 @Component({
   selector: 'app-chatbot',
@@ -12,12 +13,18 @@ import {ChatService} from "../../../services/chat/chat.service";
 })
 export class ChatbotComponent {
   message: FormControl = new FormControl();
+  response!: string;
 
-  constructor(private chatService: ChatService) {
+  constructor(private chatService: ChatService, private productsService: ProductsService) {
   }
 
   sendMessage() {
     const customerId = 1
-    this.chatService.chat(customerId, this.message.value).subscribe(r => alert(r.answer))
+    this.chatService.chat(customerId, this.message.value).subscribe(r => {
+      this.response = r.result;
+      if (r.isFinished) {
+        this.productsService.getProducts(customerId)
+      }
+    });
   }
 }
